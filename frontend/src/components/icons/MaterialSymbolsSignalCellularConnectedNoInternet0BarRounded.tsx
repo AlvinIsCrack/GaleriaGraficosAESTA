093e8db1c36db65429170c5bd0e6218bed49c157
AlvinIsCrack <animations468@gmail.com>
2026-02-01 16:20:13 -0300

    fix warns

--- frontend/src/components/icons/MaterialSymbolsSignalCellularConnectedNoInternet0BarRounded.tsx
+++ frontend/src/components/icons/MaterialSymbolsSignalCellularConnectedNoInternet0BarRounded.tsx
@@ -1,4 +1,4 @@
-import React, { type SVGProps } from 'react'
+import { type SVGProps } from 'react'
 
 export function MaterialSymbolsSignalCellularConnectedNoInternet0BarRounded(props: SVGProps<SVGSVGElement>) {
   return (
